73aec3da2ef0ac00366942d2ec25b9e14441f978
Luan <barkon32@tuta.io>
2023-05-09 11:45:38 -0300

    criado cargo como enum no user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: { user: 0, admin: 1 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_08_182724) do
+ActiveRecord::Schema.define(version: 2023_05_09_144039) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 2023_05_08_182724) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin", default: false, null: false
     t.integer "cpf", null: false
     t.string "name", null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
