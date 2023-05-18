6c765c1b75e1caca7ce5d59ce964d0dc7d0e5682
Luan <barkon32@tuta.io>
2023-05-17 22:04:23 -0300

    corrigido visualização de lotes por tempo

--- spec/system/auction_batch/user_view_auction_batch_details_spec.rb
+++ spec/system/auction_batch/user_view_auction_batch_details_spec.rb
@@ -16,7 +16,7 @@ describe 'Usuário visita Lote para Leilão' do
     within 'nav' do
       click_on 'Lotes para Leilão'
     end
-    click_on 'A4K1L9'
+    click_on 'Lote: A4K1L9'
 
     expect(page).not_to have_button('Aprovar Lote')
     expect(page).to have_content('A4K1L9')
@@ -44,18 +44,18 @@ describe 'Usuário visita Lote para Leilão' do
     within 'nav' do
       click_on 'Lotes para Leilão'
     end
-    click_on 'A4K1L9'
+    click_on 'Lote: A4K1L9'
 
     expect(page).not_to have_button('Aprovar Lote')
     expect(page).to have_content('A4K1L9')
     expect(page).to have_content('Quantidade de itens: 1')
-    expect(page).to have_content('Data de início: ' + I18n.l(auction_batch.start_date, format: :short))
-    expect(page).to have_content('Data de término: ' + I18n.l(auction_batch.end_date, format: :short))
+    expect(page).to have_content('Data de início: ' + I18n.l(2.hours.from_now, format: :short))
+    expect(page).to have_content('Data de término: ' + I18n.l(5.days.from_now, format: :short))
     expect(page).to have_content('Lance inicial: R$ 100,00')
     expect(page).to have_content('Menor diferença entre lances: R$ 10,00')
   end
 
-  it 'com sucesso' do
+  it 'como admin' do
     user = User.create!(email: 'julia@leilaodogalpao.com.br', password: '@#$GBRD', name: 'Julia', cpf: '04206205086')
     auction_item_category = AuctionItemCategory.create!(name: 'Eletrônicos')
     auction_batch = AuctionBatch.create!(code: 'A4K1L9', start_date: 2.hours.from_now, end_date: 5.days.from_now, minimum_bid_amount: 100,
@@ -70,7 +70,7 @@ describe 'Usuário visita Lote para Leilão' do
     within 'nav' do
       click_on 'Lotes para Leilão'
     end
-    click_on 'A4K1L9'
+    click_on 'Lote: A4K1L9'
 
     expect(page).not_to have_button('Aprovar Lote')
     expect(page).to have_content('A4K1L9')
--- spec/system/auction_bid/user_bids_on_auction_batch_spec.rb
+++ spec/system/auction_bid/user_bids_on_auction_batch_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'User vê lote para leilão' do
+describe 'Usuário faz lance no lote para leilão' do
   it 'não logado' do
     user = User.create!(email: 'julia@leilaodogalpao.com.br', password: '@#$GBRD', name: 'Julia', cpf: '04206205086')
     auction_item_category = AuctionItemCategory.create!(name: 'Eletrônicos')
@@ -16,31 +16,26 @@ describe 'User vê lote para leilão' do
     within 'nav' do
       click_on 'Lotes para Leilão'
     end
+    within "div#batch-#{auction_batch.id}" do
+      click_on 'A4K1L9'
+    end
+    fill_in 'Valor do lance', with: '150'
+    click_on 'Fazer lance'
 
-    expect(page).to have_content('A4K1L9')
-    expect(page).to have_content('Quantidade de itens: 1')
-    expect(page).to have_content('Data de início: ' + I18n.l(2.hours.from_now, format: :short))
-    expect(page).to have_content('Data de término: ' + I18n.l(5.days.from_now, format: :short))
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content('Faça login para poder realizar lances.')
   end
+
   it 'como visitante' do
     guest_user = User.create!(email: 'paulo@email.com', password: '171653', name: 'Paulo', cpf: '96749196004')
     user = User.create!(email: 'julia@leilaodogalpao.com.br', password: '@#$GBRD', name: 'Julia', cpf: '04206205086')
     auction_item_category = AuctionItemCategory.create!(name: 'Eletrônicos')
     auction_batch = AuctionBatch.create!(code: 'A4K1L9', start_date: 2.hours.from_now, end_date: 5.days.from_now, minimum_bid_amount: 100,
                                          minimum_bid_difference: 10, created_by_user_id: user.id)
-    travel_to(2.hour.ago)
-    second_auction_batch = AuctionBatch.create!(code: '6G42DF', start_date: 1.hour.from_now, end_date: 5.days.from_now, minimum_bid_amount: 100,
-                                                minimum_bid_difference: 10, created_by_user_id: user.id)
-    second_auction_batch.approved!
-    travel_back
     auction_item = AuctionItem.create!(name: 'TV Samsung 32', description: 'Samsung Smart TV 32 polegadas HDR LED 4K', weight: 10_000, width: 50,
                                        height: 70, depth: 10, auction_item_category_id: auction_item_category.id, auction_batch_id: auction_batch.id)
-    second_auction_item = AuctionItem.create!(name: 'TV Philips 40', description: 'Philips TV Smart 40 polegadas HDR OLED 8K', weight: 12_000, width: 60,
-                                              height: 90, depth: 10, auction_item_category_id: auction_item_category.id, auction_batch_id: second_auction_batch.id)
     auction_item.image.attach(io: File.open('spec/fixtures/tv-imagem.png'), filename: 'tv-imagem.png',
                               content_type: 'image/png')
-    second_auction_item.image.attach(io: File.open('spec/fixtures/tv-imagem.png'), filename: 'tv-imagem.png',
-                                     content_type: 'image/png')
     auction_batch.approved!
 
     login_as(guest_user)
@@ -48,22 +43,20 @@ describe 'User vê lote para leilão' do
     within 'nav' do
       click_on 'Lotes para Leilão'
     end
+    within "div#batch-#{auction_batch.id}" do
+      click_on 'A4K1L9'
+    end
 
-    within 'div#future-batches' do
-      expect(page).to have_content('A4K1L9')
-      expect(page).to have_content('Quantidade de itens: 1')
-      expect(page).to have_content('Data de início: ' + I18n.l(2.hours.from_now, format: :short))
-      expect(page).to have_content('Data de término: ' + I18n.l(5.days.from_now, format: :short))
-    end
-    within 'div#ongoing-batches' do
-      expect(page).to have_content('6G42DF')
-      expect(page).to have_content('Quantidade de itens: 1')
-      expect(page).to have_content('Data de início: ' + I18n.l(1.hour.ago, format: :short))
-      expect(page).to have_content('Data de término: ' + I18n.l(5.days.from_now - 2.hours, format: :short))
-    end
+    expect(page).not_to have_button('Aprovar Lote')
+    expect(page).to have_content('A4K1L9')
+    expect(page).to have_content('Quantidade de itens: 1')
+    expect(page).to have_content('Data de início: ' + I18n.l(auction_batch.start_date, format: :short))
+    expect(page).to have_content('Data de término: ' + I18n.l(auction_batch.end_date, format: :short))
+    expect(page).to have_content('Lance inicial: R$ 100,00')
+    expect(page).to have_content('Menor diferença entre lances: R$ 10,00')
   end
 
-  it 'com sucesso' do
+  it 'como admin' do
     user = User.create!(email: 'julia@leilaodogalpao.com.br', password: '@#$GBRD', name: 'Julia', cpf: '04206205086')
     auction_item_category = AuctionItemCategory.create!(name: 'Eletrônicos')
     auction_batch = AuctionBatch.create!(code: 'A4K1L9', start_date: 2.hours.from_now, end_date: 5.days.from_now, minimum_bid_amount: 100,
@@ -78,26 +71,16 @@ describe 'User vê lote para leilão' do
     within 'nav' do
       click_on 'Lotes para Leilão'
     end
+    click_on 'A4K1L9'
 
+    expect(page).not_to have_button('Aprovar Lote')
     expect(page).to have_content('A4K1L9')
     expect(page).to have_content('Quantidade de itens: 1')
-    expect(page).to have_content('Data de início: ' + I18n.l(2.hours.from_now, format: :short))
-    expect(page).to have_content('Data de término: ' + I18n.l(5.days.from_now, format: :short))
+    expect(page).to have_content('Data de início: ' + I18n.l(auction_batch.start_date, format: :short))
+    expect(page).to have_content('Data de término: ' + I18n.l(auction_batch.end_date, format: :short))
+    expect(page).to have_content('Lance inicial: R$ 100,00')
+    expect(page).to have_content('Menor diferença entre lances: R$ 10,00')
     expect(page).to have_content('Criado por: Julia')
     expect(page).to have_content('Status: Aguardando aprovação')
-  end
-
-  it 'e não existem lotes para leilão cadastrados' do
-    user = User.create!(email: 'julia@leilaodogalpao.com.br', password: '@#$GBRD', name: 'Julia', cpf: '04206205086')
-
-    login_as(user)
-    visit root_path
-    within 'nav' do
-      click_on 'Lotes para Leilão'
-    end
-
-    expect(page).not_to have_content('Acesso não autorizado.')
-    expect(page).to have_content('Nenhum Lote em andamento.')
-    expect(page).to have_content('Nenhum Lote futuro.')
   end
 end
